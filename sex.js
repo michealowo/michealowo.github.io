0907e6963c6084ab3e373b2a8c46a910d7316b34
michealowo <90820955+michealowo@users.noreply.github.com>
2022-12-16 22:34:40 -0800

    Update sex.js

--- sex.js
+++ sex.js
@@ -1,17 +1,12 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('@bhmb/bot')) :
-  typeof define === 'function' && define.amd ? define(['@bhmb/bot'], factory) :
-  (factory(global['@bhmb/bot']));
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('@bhmb/bot')) :
+    typeof define === 'function' && define.amd ? define(['@bhmb/bot'], factory) :
+    (factory(global['@bhmb/bot']));
 }(this, (function (bot) { 'use strict';
-  const MessageBot = bot.MessageBot
-  MessageBot.registerExtension('mikeal/sex', function(ex, world) {
-    world.addCommand('sex', function(player, args) {
-      ex.bot.send('HOLY SHIT {{Name}}')
-      ex.bot.send('OMG WHEN')
+    const MessageBot = bot.MessageBot
+    MessageBot.registerExtension('mikeal/sex', function(ex, world) {
+        world.addCommand('sex', function(player, args) {
+            ex.bot.send('SEX!')
+        })
     })
-
-    ex.remove = function() {
-      world.removeCommand('sex')
-    }
-  })
 })))
